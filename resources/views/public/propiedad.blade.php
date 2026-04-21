0c22d34b9431b43e84fd158c339ba6952eed392c
root <root@66-116-210-83.webhostbox.net>
2026-04-21 01:55:45 -0600

    fix: use correct _gallery include instead of _gallery-premium
    Changed all includes from _gallery-premium to _gallery to use the
    working vanilla JS gallery without external dependencies.

--- resources/views/public/propiedad.blade.php
+++ resources/views/public/propiedad.blade.php
@@ -167,7 +167,7 @@
     {{-- Gallery carousel --}}
     <div class="bg-gray-100 pt-20">
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-6">
-            @include('public.propiedad._gallery-premium', ['property' => $property])
+            @include('public.propiedad._gallery', ['property' => $property])
         </div>
     </div>
 
@@ -243,7 +243,7 @@
                 {{-- Main content --}}
                 <div class="lg:col-span-2">
                     {{-- Image Carousel --}}
-                    @include('public.propiedad._gallery-premium', ['property' => $property])
+                    @include('public.propiedad._gallery', ['property' => $property])
 
                     @include('public.propiedad._share', ['property' => $property])
 
