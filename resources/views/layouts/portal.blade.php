ebe28e69efb9848592cf7050bba2478374752c6b
Alejandro <lechugadesign@gmail.com>
2026-06-30 16:57:38 -0600

    Center logo in portal sidebar

--- resources/views/layouts/portal.blade.php
+++ resources/views/layouts/portal.blade.php
@@ -67,6 +67,7 @@
         .sb-logo a {
             display: flex;
             align-items: center;
+            justify-content: center;
             gap: .75rem;
             width: 100%;
         }
